b1286a3b4f51a0db9dff200443019aff7a6953da
Joey Bae <97127372+JoeyBae93@users.noreply.github.com>
2025-03-01 15:10:24 -0500

    update

--- JoeyB_Chatbot_FrontEnd/src/App.jsx
+++ JoeyB_Chatbot_FrontEnd/src/App.jsx
@@ -1,24 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
 function App() {
   const handleSubmit = async () => {
     const messageInput = document.getElementById('messageInput')
     if (!messageInput) return
     const userMessage = messageInput.value
-    messageInput.value= ''
+    messageInput.value = ''
 
-    const message = JSON.stringify({role: 'user', content: userMessage})
+    const message = JSON.stringify({role: 'user', content: userMessage })
     console.log("User: " + userMessage)
     let response = null
-
     try {
       response = await fetch('http://localhost:5000/chatbot', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: message,
       })
@@ -26,19 +25,19 @@ function App() {
         console.error('Error:', error)
         return
     }
-
     const data = await response.json()
-    console.log("Assistant: " + data.response)
 
-    return (
-      <>
-        <div id="prompt">
-          <input id="messageInput" contentEditable></input>
-          <button id="submit" onClick={handleSubmit}></button>
-        </div>
-      </>
-    )
+    console.log("Assistant: " + data.response)
   }
+
+  return (
+    <>
+      <div id="prompt">
+        <input id="messageInput" contentEditable></input>
+        <button id="submit" onClick={handleSubmit}></button>    
+      </div>
+    </>
+  )
 }
 
 export default App
